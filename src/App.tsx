bd2bdbb5abbfab2e7bb228c70864ebaaab269726
Me_Not404 <m3not404@gmail.com>
2026-02-07 21:53:39 +0000

    feat: add ThemeProvider with localStorage persistence

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css'
-
+import { ThemeProvider } from './contexts/theme/ThemeProvider'
+import { ModeToggle } from './components/layout/ModeToggle'
 function App() {
 
   return (
     <>
-      
+      <ThemeProvider  defaultTheme="light" storageKey="vite-ui-theme">
+        <div>
+          <ModeToggle/>
+          <h1>FasoClass</h1>
+        </div>
+      </ThemeProvider>
     </>
   )
 }
