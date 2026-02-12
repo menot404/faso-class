3580a669d836ebc658ed19fb876364cd7528fc68
Me_Not404 <m3not404@gmail.com>
2026-02-12 21:16:18 +0000

    refactor(auth): enhance authentication context and hook

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,12 +1,10 @@
-import { useContext } from 'react';
-import { AuthContext } from '../contexts/auth/AuthContext';
+import { useContext } from 'react'
+import { AuthContext } from '@/contexts/auth/AuthContext' 
 
-export function useAuth() {
-  const context = useContext(AuthContext);
-  
-  if (context === undefined) {
-    throw new Error('useAuth must be used within an AuthProvider');
+export const useAuth = () => {
+  const context = useContext(AuthContext)
+  if (!context) {
+    throw new Error('useAuth must be used within AuthProvider')
   }
-  
-  return context;
+  return context
 }
