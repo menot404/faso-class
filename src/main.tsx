3dfea08c0c8334c1e7dd72be98953372e924c8bb
Me_Not404 <m3not404@gmail.com>
2026-02-25 23:22:06 +0000

    feat: integrate Framer Motion animations in app
    - Add AnimatedPage transitions
    - Configure motion providers

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { QueryProvider } from './providers/QueryProvider.tsx'
+import { ThemeProvider } from './contexts/theme/ThemeProvider.tsx'
+import { AuthProvider } from './contexts/auth/AuthProvider.tsx'
+import { I18nProvider } from './providers/I18nProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryProvider>
+      <ThemeProvider defaultTheme="system" storageKey="faso-theme">
+        <I18nProvider>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </I18nProvider>
+      </ThemeProvider>
+    </QueryProvider>
   </StrictMode>,
 )
